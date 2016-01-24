22052284d16eb27cc97f161190e6e8fb77f47b2e
张志豪 <izhangzhihao@hotmail.com>
2016-01-24 22:31:24 +0800

    增加了BigFileUploadController的单元测试

---
+++ FileUpload.Tests/Controllers/BigFileUploadControllerTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FileUpload.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Moq;
+
+namespace FileUpload.Controllers.Tests
+{
+    [TestClass()]
+    public class BigFileUploadControllerTests
+    {
+        [TestMethod()]
+        public void IndexTest()
+        {
+            //Arrange
+            BigFileUploadController bigFileUploadController = new BigFileUploadController();
+            //Act
+            ActionResult result = bigFileUploadController.Index() as ActionResult;
+            //Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod()]
+        public void IsMD5ExistTest()
+        {
+            #region
+            ////Arrange
+            //var bigFileUploadController = new Mock<BigFileUploadController>();
+            //bigFileUploadController.Setup(a=>a.IsMD5Exist())
+            ////Act
+            //ActionResult result = bigFileUploadController.IsMD5Exist("ef13915dda1f0dd3e2c5576ca2c7dce2", "fileName", "FileID") as ActionResult;
+
+            ////Assert
+
+            #endregion
+
+
+
+
+            //Arrange
+            BigFileUploadController bigFileUploadController = new BigFileUploadController();
+            //Act
+            ActionResult result = bigFileUploadController.IsMD5Exist("ef13915dda1f0dd3e2c5576ca2c7dce2", "fileName", "FileID") as ActionResult;
+
+            //Assert  失败： System.Web.Mvc.HttpNotFoundResult 成功： System.Web.Mvc.JsonResult
+            string a = result.ToString();
+            Assert.AreEqual(a, "System.Web.Mvc.JsonResult");
+        }
+    }
+}

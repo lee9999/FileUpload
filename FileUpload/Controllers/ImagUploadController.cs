8d0d3ce382f2bbdca20e75649f55e4358cca5b54
张志豪 <izhangzhihao@hotmail.com>
2016-01-24 18:28:43 +0800

    解耦和主要方法，分为几个小方法，方便单元测试

--- FileUpload/Controllers/ImagUploadController.cs
+++ FileUpload/Controllers/ImagUploadController.cs
@@ -19,17 +19,14 @@ namespace FileUpload.Controllers
         public ActionResult ImagUp(string id, string name, string type, string lastModifiedDate, int size, HttpPostedFileBase file)
         {
             #region 判断文件后缀名
-            //做文件类型验证 文件后缀名统一转为小写
             string ex = Path.GetExtension(file.FileName).ToLower();
-            //gif,jpg,jpeg,bmp,png
-            if (ex != ".gif" && ex != ".jpg" && ex != ".jpeg" && ex != ".bmp" && ex != ".png")
+            if (!IsFileExAcceptable(ex))
             {
-                //文件后缀不符合要求
-                //return HttpNotFound();
-                //return Json(new { jsonrpc = 2.0, error = new { code = 102, message = "保存失败" }, id = id });//这个错误处理不管用
-                return Json(new { error = true });//新的错误返回方式，更加轻量！
+                return Json(new { error = true });
             }
             #endregion
+
+
 
             #region 或者： 验证文件类型file.ContentType,其实也是验证文件后缀
             //if (file.ContentType != "image/gif" && file.ContentType != "image/jpg" && file.ContentType != "image/jpeg" && file.ContentType != "image/bmp" && file.ContentType != "image/png")
@@ -44,31 +41,18 @@ namespace FileUpload.Controllers
             #region 真正验证文件类型！改后缀也不行
 
             //FileStream fs = (FileStream)file.InputStream;
-
-
-
             Stream fs = file.InputStream;
-            string fileClass = string.Empty;
             BinaryReader br = new BinaryReader(fs);
-            byte buffer;
-            byte[] b = new byte[2];
-            buffer = br.ReadByte();
-            b[0] = buffer;
-            fileClass = buffer.ToString();
-            buffer = br.ReadByte();
-            b[1] = buffer;
-            fileClass += buffer.ToString();
-
-            //br.Close(); 释放资源在finally语句块中，提前释放会将file的内容清空
-            //fs.Close();
-            if (fileClass != "7173" && fileClass != "255216" && fileClass != "6677" && fileClass != "13780") //jpe和jpeg fileclass相同
+            if (!IsRealImag(fs,br))
             {
-                //return HttpNotFound();//jsonrpc更加轻量，似乎我的用法有问题，解决不了
-                //return Json(new { jsonrpc = 2.0, error = new { code = 102, message = "保存失败" }, id = id });//这个错误处理不管用
-                return Json(new {error=true});//新的错误返回方式，更加轻量！
+                return Json(new { error = true });
             }
+            
 
             #endregion
+
+
+
             //开始保存文件
             string filePathName = String.Empty;
             string localPath = Path.Combine(HttpRuntime.AppDomainAppPath, "Upload");
@@ -107,5 +91,56 @@ namespace FileUpload.Controllers
             });
 
         }
+
+        public bool IsFileExAcceptable(string ex)
+        {
+            //做文件类型验证 文件后缀名统一为小写
+            
+            //gif,jpg,jpeg,bmp,png
+            if (ex != ".gif" && ex != ".jpg" && ex != ".jpeg" && ex != ".bmp" && ex != ".png")
+            {
+                //文件后缀不符合要求
+               
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// 真正的通过fileclass验证是否是真的图片，改后缀名也能验证出来
+        /// </summary>
+        /// <param name="fs"></param>
+        /// <param name="br"></param>
+        /// <returns></returns>
+        public bool IsRealImag(Stream fs, BinaryReader br)
+        {
+            //Stream fs = file.InputStream;
+            string fileClass = string.Empty;
+            //BinaryReader br = new BinaryReader(fs);
+            byte buffer;
+            byte[] b = new byte[2];
+            buffer = br.ReadByte();
+            b[0] = buffer;
+            fileClass = buffer.ToString();
+            buffer = br.ReadByte();
+            b[1] = buffer;
+            fileClass += buffer.ToString();
+
+            //br.Close(); 释放资源在finally语句块中，提前释放会将file的内容清空
+            //fs.Close();
+            if (fileClass != "7173" && fileClass != "255216" && fileClass != "6677" && fileClass != "13780") //jpe和jpeg fileclass相同
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        
     }
 }

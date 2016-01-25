b9c78324161bd22f5b0bdc09db2aa601f30778e5
张志豪 <izhangzhihao@hotmail.com>
2016-01-25 22:10:59 +0800

    急死我啦，怎么回事啊

--- build.cmd
+++ build.cmd
@@ -16,13 +16,4 @@ copy %CACHED_NUGET% .nuget\nuget.exe > nul
 
 :restore
 IF EXIST build goto run
-IF "%BUILDCMD_KOREBUILD_VERSION%"=="" (
-    .nuget\nuget.exe install KoreBuild -ExcludeVersion -o packages -nocache -pre
-) ELSE (
-    .nuget\nuget.exe install KoreBuild -version %BUILDCMD_KOREBUILD_VERSION% -ExcludeVersion -Out packages -nocache -pre
-)
-xcopy packages\KoreBuild\build build\ /Y
-.nuget\NuGet.exe install Sake -ExcludeVersion -Source https://www.nuget.org/api/v2/ -Out packages
-
-:run
-packages\Sake.0.2\tools\Sake.exe -I build -f makefile.shade %*
+@msbuild build.msbuild %*

1be95b934192350d7c7ddd553c358636f5ca909d
张志豪 <izhangzhihao@hotmail.com>
2016-01-26 12:53:59 +0800

    test

--- build.cmd
+++ build.cmd
@@ -1,17 +1,3 @@
-@echo off
-cd %~dp0
-
-SETLOCAL
-SET CACHED_NUGET=%LocalAppData%\NuGet\NuGet.exe
-
-IF EXIST %CACHED_NUGET% goto copynuget
-echo Downloading latest version of NuGet.exe...
-IF NOT EXIST %LocalAppData%\NuGet md %LocalAppData%\NuGet
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest 'https://www.nuget.org/nuget.exe' -OutFile '%CACHED_NUGET%'"
-
-:copynuget
-IF EXIST .nuget\nuget.exe goto restore
-md .nuget
-copy %CACHED_NUGET% .nuget\nuget.exe > nul
-
-:restore
+@echo OFF
+msbuild /p:Configuration=Release /t:Rebuild
+ECHO "FileUpload.dll is in FileUpload/bin/Release/"

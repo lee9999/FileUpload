d524c6b72a74eae9e7168c5efb549de2278aeda3
张志豪 <izhangzhihao@hotmail.com>
2016-03-12 11:24:02 +0800

    添加了xUnit的单元测试

---
+++ FileUpload.xUnit.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+using FileUpload.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace FileUpload.Controllers.xUnit.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact(DisplayName = "对HomeController的Index的测试")]
+        public void IndexTest()
+        {
+            HomeController homeController = new HomeController();
+            ViewResult result=homeController.Index() as ViewResult;
+            Assert.NotNull(result);
+        }
+    }
+}

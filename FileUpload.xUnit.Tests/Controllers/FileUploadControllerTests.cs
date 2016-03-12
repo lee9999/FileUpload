64bbc70e92b98c325943ce0f487294f61a067077
张志豪 <izhangzhihao@hotmail.com>
2016-03-12 14:29:38 +0800

    添加了部分单元测试

---
+++ FileUpload.xUnit.Tests/Controllers/FileUploadControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+using FileUpload.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace FileUpload.Controllers.xUnit.Tests
+{
+    public class FileUploadControllerTests
+    {
+        [Fact()]
+        public void IndexTest()
+        {
+            FileUploadController fileUploadController = new FileUploadController();
+            ViewResult result = fileUploadController.Index() as ViewResult;
+            Assert.NotNull(result);
+        }
+    }
+}
---
+++ FileUpload.xUnit.Tests/Controllers/ImagUploadControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+using FileUpload.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace FileUpload.Controllers.xUnit.Tests
+{
+    public class ImagUploadControllerTests
+    {
+        [Fact()]
+        public void IndexTest()
+        {
+            ImagUploadController imagUploadController = new ImagUploadController();
+            ViewResult result = imagUploadController.Index() as ViewResult;
+            Assert.NotNull(result);
+        }
+    }
+}
